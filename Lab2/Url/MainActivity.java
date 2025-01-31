d87c313dfe54ed2dc85d679e879fb42eb13809d7
Uppaluri Jahnavi <131586556+cygnus06@users.noreply.github.com>
2025-01-31 22:51:00 +0530

    Update MainActivity.java

--- Lab2/Url/MainActivity.java
+++ Lab2/Url/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.app1;
 
-import javax.swing.text.View;
+import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
