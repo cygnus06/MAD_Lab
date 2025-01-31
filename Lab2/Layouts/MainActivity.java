fbb57ead7c921cbee91a3cc5fd2b04465ebf0998
Uppaluri Jahnavi <131586556+cygnus06@users.noreply.github.com>
2025-01-31 23:26:07 +0530

    Update MainActivity.java

---
+++ Lab2/Layouts/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.app2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content view to the LinearLayout layout
+        setContentView(R.layout.activity_main);
+    }
+}
