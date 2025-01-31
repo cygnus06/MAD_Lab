c25ef2317fbc8b4e0fdcaab7eb9add4837e27fd4
Uppaluri Jahnavi <131586556+cygnus06@users.noreply.github.com>
2025-01-31 22:54:06 +0530

    Update SecondActivity.java

--- Lab2/Que1/SecondActivity.java
+++ Lab2/Que1/SecondActivity.java
@@ -1,9 +1,8 @@
 package com.example.app1;
 
-import javax.swing.text.View;
-
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,7 +13,7 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second); // Use the correct XML file
+        setContentView(R.layout.activity_second); // Ensure this XML file exists
 
         Log.d(TAG, "SecondActivity: onCreate() called");
 
@@ -22,38 +21,8 @@ public class SecondActivity extends AppCompatActivity {
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish(); // Closes SecondActivity and returns to MainActivity
+                finish(); // Close SecondActivity and return to MainActivity
             }
         });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "SecondActivity: onStart() called");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "SecondActivity: onResume() called");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "SecondActivity: onPause() called");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "SecondActivity: onStop() called");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "SecondActivity: onDestroy() called");
     }
 }
