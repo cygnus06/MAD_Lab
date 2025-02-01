12f052b46de11a45f8985367b141d60ba4425609
Uppaluri Jahnavi <131586556+cygnus06@users.noreply.github.com>
2025-02-01 07:14:30 +0530

    Add files via upload

---
+++ Lab3/news/MainActivity.java
@@ -0,0 +1,5 @@
+// implementation 'androidx.viewpager2:viewpager2:1.1.0'
+//implementation 'com.google.android.material:material:1.9.0'
+//<activity android:name=".MainFragment"
+//     tools:ignore="Instantiatable" />
+//
